cee99566bfafb6e80f5eb52440cfe2dd6d492808
“cjlopez-dev” <93419901+cjlopez-dev@users.noreply.github.com>
2026-03-20 14:38:25 -0600

    Adding Required Email to Dto Model

--- Cryptiq/Dtos/LoginDto.cs
+++ Cryptiq/Dtos/LoginDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Cryptiq.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cryptiq.Dtos
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
     }
 
